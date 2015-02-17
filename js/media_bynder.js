9313c4445c685430c63638ee79423970b4aa07aa
Michiel Westerbeek <happylinks@gmail.com>
2015-02-17 17:50:45 +0100

    tmp fix for iframe problem

--- js/media_bynder.js
+++ js/media_bynder.js
@@ -57,5 +57,26 @@
                 }
             });
         });
+        var params = getSearchParameters();
+        if(params.iframe){
+            $('body > :not(#media-bynder-add)').hide();
+            $('#media-bynder-add').appendTo('body');
+        }
     });
 })(jQuery);
+
+
+function getSearchParameters() {
+      var prmstr = window.location.search.substr(1);
+      return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
